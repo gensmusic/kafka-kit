f7e876e6452c6d2999d73f63d173b805bb2690a1
gensmusic <gensmusic@163.com>
2020-07-06 22:16:51 +0800

    add clap

--- src/bin/producer.rs
+++ src/bin/producer.rs
@@ -1,3 +1,28 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("hello world");
+    let matches = App::new("producer")
+        .version("0.1")
+        .author("gensmusic <gensmusic@163.com>")
+        .about("kafka producer")
+        .arg(
+            Arg::new("brokers")
+                .long("brokers")
+                .value_name("brokers")
+                .default_value("127.0.0.1:9092")
+                .about("brokers of kafka")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::new("topic")
+                .value_name("topic")
+                .about("topic you want to produce")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+
+
+    println!("{:?}", matches);
 }
